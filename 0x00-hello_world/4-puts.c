16a9a84a7c17f7c9be06b1ad7fcc4c9cbf344d7a
Zineb Belassiri <zbelassiri@gmail.com>
2023-04-20 09:06:59 -0700

    4-puts.c added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - puts "Programming is like building a multilingual puzzle" folliwed by new line
+ **/
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
