ef31dcb0a9954dcbd35c9cdb9fa90cf3241dd404
Zineb Belassiri <zbelassiri@gmail.com>
2023-05-02 12:34:12 -0700

    5-rev_string.c added

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: chat pointer.
+ * Return: nothing.
+ */
+void rev_string(char *s)
+{
+	char x = s[0];
+	int length;
+	int i;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+		for (i = 0; i < length; i++)
+		{
+			length--;
+			x = s[i];
+			s[i] = s[length];
+			s[length] = x;
+		}
+	}
+}
+
