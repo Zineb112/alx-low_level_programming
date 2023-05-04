04313b659a2a07dddf104871caafb6595c625f3b
Zineb Belassiri <zbelassiri@gmail.com>
2023-05-04 07:21:47 -0700

    0-strcat.c added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * *_strcat - appends the src string to the dest string,
@@ -10,5 +11,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int x;
+	int y;
 
+	x = 0;
+	y = 0;
+
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		y++;
+		x++;
+	}
+	dest[x] = '\0';
+
+	return (dest);
 }
