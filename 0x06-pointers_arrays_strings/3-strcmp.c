c0b72e1a87eb5e22644dfc97552e820d5536156f
Zineb Belassiri <zbelassiri@gmail.com>
2023-05-04 16:15:28 -0700

    3-strcmp.c added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,21 +11,14 @@ int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
-		if (*s1 == '\0' || *s2 == '\0')
+		if (*s1 == '\0')
 		{
-			break;
+			return (0);
 		}
 
 		s1++;
 		s2++;
 	}
-	if (*s1 == '\0' && *s2 == '\0')
-	{
-		return (0);
-	}
-	else
-	{
-		return (-1);
 
-	}
+	return (*s1 - *s2);
 }
