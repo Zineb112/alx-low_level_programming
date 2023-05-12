90f8e6ab0acd6a4618466dd2e28aab009770e906
Zineb Belassiri <zbelassiri@gmail.com>
2023-05-12 08:23:42 -0700

    0-whatsmyname.c edited

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,9 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("%s", argv[0]);
-	printf("\n");
+	int count;
 
+	for (count = 0; count < argc; count++)
+		printf("%s\n", argv[count]);
 	return (0);
 }
