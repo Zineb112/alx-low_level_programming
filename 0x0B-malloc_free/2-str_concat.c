3ca1e968d3802aad8ad011ec08816f92786b8d22
Zineb Belassiri <zbelassiri@gmail.com>
2023-05-15 12:58:55 -0700

    2-str_concat.c edited

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,23 +26,25 @@ char *str_concat(char *s1, char *s2)
 	{
 		;
 	}
-
 	for (y = 0; s2[y] != '\0'; y++)
 	{
 		;
 	}
-
 	ptr = malloc(sizeof(char) * (x + y + 1));
-
 	if (ptr == NULL)
 	{
 		free(ptr);
 		return (0);
 	}
-
 	for (z = 0; z < x; z++)
 	{
 		ptr[z] = s1[z];
+	}
+
+	i = y;
+	for (y = 0; y <= i; z++, y++)
+	{
+		ptr[z] = s2[y];
 	}
 
 	return (ptr);
