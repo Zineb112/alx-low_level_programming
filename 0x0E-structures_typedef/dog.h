19a1670263e827f699b20f2b94b3889e06e0dce0
Zineb Belassiri <zbelassiri@gmail.com>
2023-05-22 13:46:05 -0700

    dog.h added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - a struct.
+ * @name: poppy's name.
+ * @age: poppy's age.
+ * @owner: poppy's owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
