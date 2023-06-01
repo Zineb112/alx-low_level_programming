d00c0e1ed1f43a364d226ee5b78b92e1e34813dc
Zineb112 <zbelassiri@gmail.com>
2023-06-01 15:37:45 -0700

    100-first.c error fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,8 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints You're beat! and yet, you must 
- * allow,\nI bore my house upon my back!\n 
+ * first - prints You're beat! and yet, you must
+ * allow,\nI bore my house upon my back!\n
  * before the main function is executed.
  */
 void first(void)
