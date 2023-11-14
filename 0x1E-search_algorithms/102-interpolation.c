8c43917d9326c96323ce593e576c5cf10b73b00b
Zineb112 <zbelassiri@gmail.con>
2023-11-14 12:41:52 -0800

    102-interpolation.c added

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,49 +1,52 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array of integers
- * using the Binary search algorithm.
- * @array: Pointer to the first element of the array to search in.
- * @size: Number of elements in the array.
- * @value: Value to search for.
+ * interpolation_search - searches for a value in an array of
+ * integers using the Interpolation search algorithm
  *
- * Return: The index where the value is located,
- * or -1 if the value is not present in the array or if the array is NULL.
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
-int binary_search(int *array, size_t size, int value)
+int interpolation_search(int *array, size_t size, int value)
 {
-    size_t left = 0, right = size - 1, mid;
+	size_t pos, low, high;
+	double f;
 
-    if (array == NULL)
-        return -1;
+	if (array == NULL)
+		return (-1);
 
-    while (left <= right)
-    {
-        /* Print the current subarray being searched */
-        printf("Searching in array: ");
-        for (size_t i = left; i <= right; i++)
-        {
-            if (i < right)
-                printf("%d, ", array[i]);
-            else
-                printf("%d", array[i]);
-        }
-        printf("\n");
+	low = 0;
+	high = size - 1;
 
-        mid = left + (right - left) / 2;
+	while (size)
+	{
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
+		printf("Value checked array[%d]", (int)pos);
 
-        /* Check if the value is present at the middle of the array */
-        if (array[mid] == value)
-            return mid;
+		if (pos >= size)
+		{
+			printf(" is out of range\n");
+			break;
+		}
+		else
+		{
+			printf(" = [%d]\n", array[pos]);
+		}
 
-        /* If the value is smaller, ignore the right half */
-        if (array[mid] > value)
-            right = mid - 1;
-        /* If the value is larger, ignore the left half */
-        else
-            left = mid + 1;
-    }
+		if (array[pos] == value)
+			return ((int)pos);
 
-    return -1; /* Value not found */
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+
+		if (low == high)
+			break;
+	}
+
+	return (-1);
 }
-
